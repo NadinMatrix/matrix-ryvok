0515a15ed044305e5d0e39eb61b43b78463fbbb5
NadinMatrix <n.fadievska@gmail.com>
2025-11-13 10:49:49 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -234,7 +234,6 @@ export default function Home() {
             >
               ✦ Результат ✦
             </div>
-
             <pre
               style={{
                 whiteSpace: 'pre-wrap',
